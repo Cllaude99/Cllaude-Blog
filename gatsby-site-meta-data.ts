308df778752d6ca9c309fa7aa7f33a6e2057eb62
Cllaude99 <xodbsrla1025@naver.com>
2025-03-28 19:32:52 +0900

    chore: update about

--- gatsby-site-meta-data.ts
+++ gatsby-site-meta-data.ts
@@ -71,7 +71,7 @@ export default {
     },
     {
       category: 'Activity',
-      date: '2025.03 ~',
+      date: '2025.03 ~ ',
       en: 'Farm System',
       kr: 'Farm System 보안/웹',
       info: '교내 자율 학습 동아리',
@@ -79,7 +79,7 @@ export default {
     },
     {
       category: 'Activity',
-      date: '2025.03 ~',
+      date: '2025.03 ~ ',
       en: '9roomthon Univ',
       kr: '9roomthon Univ',
       info: 'IT 연합 동아리',
